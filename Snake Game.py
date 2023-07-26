ac42d420f03900690a43e1261bb2bfe9826c421d
bitmap357 <edakoyeram@gmail.com>
2023-07-26 13:55:02 +0000

    getting the window to stay up

--- Snake Game.py
+++ Snake Game.py
@@ -8,3 +8,11 @@ window_height = 600
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Snake Game")
 
+
+
+game_over = False
+
+while not game_over:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_over = True
