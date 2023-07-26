f25596dd2200babc5aacdf8b37fd0ae57a24adba
bitmap357 <edakoyeram@gmail.com>
2023-07-26 14:10:33 +0000

    drawing the rectangle

--- Snake Game.py
+++ Snake Game.py
@@ -9,10 +9,11 @@ window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Snake Game")
 
 
-
 game_over = False
 
 while not game_over:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_over = True
+
+    pygame.draw.rect(window, )
