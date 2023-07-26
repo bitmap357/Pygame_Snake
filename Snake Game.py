f8288e589d3769d9da99996faf9fcbad62095fe8
bitmap357 <edakoyeram@gmail.com>
2023-07-26 16:24:39 +0000

    snake head, snake body

--- Snake Game.py
+++ Snake Game.py
@@ -64,6 +64,8 @@ while not game_over:
     snake_head.append(x1)
     snake_head.append(y1)
 
+    snake_body.append(snake_head)
+
     if x1 == foodx and y1 == foody:
         foodx = round(random.randrange(0, window_width - 10) / 10) * 10.0
         foody = round(random.randrange(0, window_height - 10) / 10) * 10.0
