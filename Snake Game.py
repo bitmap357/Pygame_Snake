dc55234d1201e0500a0661cb7bfd4429367ec954
edako <edakoyeram@gmail.com>
2023-07-29 23:21:53 +0000

    comment

--- Snake Game.py
+++ Snake Game.py
@@ -37,6 +37,7 @@ foodx = round(random.randrange(0, window_width - 10) / 10) * 10.0
 foody = round(random.randrange(0, window_height - 10) / 10) * 10.0
 
 
+# clock for the game
 clock = pygame.time.Clock()
 
 while not game_over:
