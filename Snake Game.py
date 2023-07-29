32e32177eb0c5d13d80f251caf17201ae1d302a9
edako <edakoyeram@gmail.com>
2023-07-29 23:20:06 +0000

    comment

--- Snake Game.py
+++ Snake Game.py
@@ -11,6 +11,7 @@ window_height = 600
 window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("Snake Game")
 
+# Setting colors
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
